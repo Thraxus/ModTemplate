09850ce9904a17002cc45f739e88686a267bacf9
Thraxus <mornadak@hotmail.com>
2020-05-27 18:48:44 -0400

    Tweaked the log!

--- Mod-ModID/Data/Scripts/Namespace/Common/BaseClasses/LogBaseEvent.cs
+++ Mod-ModID/Data/Scripts/Namespace/Common/BaseClasses/LogBaseEvent.cs
@@ -13,9 +13,9 @@ namespace ModTemplate.Namespace.Common.BaseClasses
 			_id = id;
 		}
 
-		public void WriteToLog(string caller, string message, LogType logType)
+		protected void WriteToLog(string caller, string message, LogType logType)
 		{
-			OnWriteToLog?.Invoke(string.IsNullOrWhiteSpace(_id) ? caller : $"{caller} ({_id})", message, logType);
+			OnWriteToLog?.Invoke($"{_id}: {caller}", message, logType);
 		}
 	}
 }
